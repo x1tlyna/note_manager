0e73099be9a34b4d75edb1752b926714ed59d018
x1tlyna <76436018+x1tlyna@users.noreply.github.com>
2024-12-17 15:44:44 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,30 @@
+from datetime import datetime, timedelta
+current_datetime = datetime.now()
+
+
 username=input('введите имя пользователя: ') #ввод имени пользователя
 count=int(input('сколько заметок Вы хотите создать?:'))
+
 notes=[] # пустой список для заметок
+
 for a in range(count):
     title=input('введите заголовок заметки: ')
     content=input('введите пункты заметки (пункты вводить через запятую): ')
     status=input('введите статус заметки: ')
-    current_date=input('введите дату (в формате DD-MM-YYYY) начала выполнения заметки: ')
-    issue_date=input('введите окончательную дату (в формате DD-MM-YYYY) для завершения замметки: ')
+    created_date=current_datetime.date()
+    time=int(input('количество дней на выполнение заметки: '))
+    issue_date = created_date + timedelta(days=time)
     note = { #наполнение списка заметокк
         'имя': username,
         'заголовок': title,
         'пункты': content,
         'статус': status,
-        'дата начала': current_date,
-        'дата окончания': issue_date
+        'дата создания заметки': created_date.strftime('%d-%m-%Y'),
+        'срок выполнения заметки': issue_date.strftime('%d-%m-%Y')
     }
-    notes.append(note)
+    notes.append(note) # добавления значений в заметки
 print("ваши заметки:")
 for note in notes: #вывод всех получившихся заметок
-    print(note)
+    print(f"Имя: {note['имя']}, Заголовок: {note['заголовок']}, Пункты: {note['пункты']}, "
+          f"Статус: {note['статус']}, Дата создания: {note['дата создания заметки']}, "
+          f"Срок выполнения до: {note['срок выполнения заметки']}")
